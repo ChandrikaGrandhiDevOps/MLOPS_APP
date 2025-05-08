a459ac3d5efff998cfad12723ffb3132bd7f4cd3
Chandrika <grandhichandrika4@gmail.com>
2025-05-07 19:43:12 -0500

    project template ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="ChandrikaGrandhiDevOps",
+    author_email="chandrikagrandhi4@gmail.com",
+    packages=find_packages()
+)

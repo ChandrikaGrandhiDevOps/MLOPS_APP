7df517f68edf95c9f3c6530f93e69f2cfcee70d4
Chandrika <grandhichandrika4@gmail.com>
2025-05-09 19:31:59 -0500

    data ingestion done

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="src",
     version="0.0.1",
     author="ChandrikaGrandhiDevOps",
-    author_email="chandrikagrandhi4@gmail.com",
+    author_email="grandhichandrika4@gmail.com",
     packages=find_packages()
 )
